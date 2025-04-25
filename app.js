f7ab267f60aa3f835cd0feac624947b378a9b097
zlevin1228 <zacharylevin1228@gmail.com>
2025-04-25 13:48:56 -0400

    almost done with content!

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
- // When the user clicks on the button, scroll to the top of the document
- function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      infoItems: [],
+      currentIndex: 0
+    };
+  },
+  mounted() {
+    fetch('info.json')
+      .then(response => response.json())
+      .then(data => {
+        this.infoItems = data;
+        this.autoRotate();
+      });
+  },
+  methods: {
+    autoRotate() {
+      setInterval(() => {
+        this.currentIndex = (this.currentIndex + 1) % this.infoItems.length;
+      }, 5000); // every 5 seconds
+    }
   }
-  
+}).mount('#app');
