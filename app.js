df07b64fc46498406b67cc39ed943e638cb5eec7
zlevin1228 <zacharylevin1228@gmail.com>
2025-04-28 14:59:42 -0400

    Initial Prototype - Basic Structure and Navigation

--- app.js
+++ app.js
@@ -23,3 +23,9 @@ createApp({
     }
   }
 }).mount('#app');
+
+ // When the user clicks on the button, scroll to the top of the document
+ function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
